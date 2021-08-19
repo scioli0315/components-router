2702fea4a7cc3e26b19423429c1f156940d3e03a
zhuyk <zhuyk@taiyi-tech.com>
2021-08-19 17:33:50 +0800

    destructuring useQuery

--- playground/views/QueryParams.vue
+++ playground/views/QueryParams.vue
@@ -6,39 +6,43 @@ export default defineComponent({
   setup() {
     const query = useQuery<{ name: string }>()
 
-    return () => (
-      <div>
-        <h2>Accounts</h2>
+    return () => {
+      const { name } = query.value
 
-        <ul>
-          <li>
-            <RouterLink to="account?name=netflix">Netflix</RouterLink>
-          </li>
-          <li>
-            <RouterLink to="account?name=zillow-group">Zillow Group</RouterLink>
-          </li>
-          <li>
-            <RouterLink to={{ pathname: 'account', query: { name: 'yahoo' } }}>Yahoo</RouterLink>
-          </li>
-          <li>
-            <RouterLink to={{ pathname: 'account', query: { name: 'modus-create' } }}>
-              Modus Create
-            </RouterLink>
-          </li>
-        </ul>
-
+      return (
         <div>
-          {query.value.name ? (
-            <h3>
-              The <code>name</code> in the query string is &quot;{query.value.name}
-              &quot;
-            </h3>
-          ) : (
-            <h3>There is no name in the query string</h3>
-          )}
+          <h2>Accounts</h2>
+
+          <ul>
+            <li>
+              <RouterLink to="account?name=netflix">Netflix</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="account?name=zillow-group">Zillow Group</RouterLink>
+            </li>
+            <li>
+              <RouterLink to={{ pathname: 'account', query: { name: 'yahoo' } }}>Yahoo</RouterLink>
+            </li>
+            <li>
+              <RouterLink to={{ pathname: 'account', query: { name: 'modus-create' } }}>
+                Modus Create
+              </RouterLink>
+            </li>
+          </ul>
+
+          <div>
+            {name ? (
+              <h3>
+                The <code>name</code> in the query string is &quot;{name}
+                &quot;
+              </h3>
+            ) : (
+              <h3>There is no name in the query string</h3>
+            )}
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
 })
 </script>
